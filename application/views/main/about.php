920efaa85e7434be64235ec2363cf648735a1cad
daanangp <danang.priambodo66@gmail.com>
2020-11-23 19:12:48 +0700

    Add to github Aplikasi Alumni

---
+++ application/views/main/about.php
@@ -0,0 +1,66 @@
+<div class="container">
+	<div class="row">
+		<a class="line"><h4 class="margin h1" data-aos="zoom-out" data-aos-duration="500">Tentang Aplikasi</h4></a>
+        <p class="justify" data-aos="zoom-in-down" data-aos-duration="500">Ini adalah Sistem Informasi Alumni berbasis web, 
+
+        </p>
+        <p class="justify" data-aos="zoom-in-down" data-aos-duration="500">
+
+        </p>
+        <table class="justify" data-aos="zoom-in-down" data-aos-duration="500">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Keterangan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Framework / versi</td>
+                        <td><a href="https://github.com/bcit-ci/CodeIgniter" target="_blank">Codeigniter / <?php echo CI_VERSION; ?></a></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Template Depan</td>
+                        <td><a href="https://github.com/tiagosousaeti/materialize-landing-page" target='_blank'>materialize-landing-page</a></td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Template cpanel</td>
+                        <td><a href="https://github.com/gurayyarar/AdminBSBMaterialDesign" target='_blank'>AdminBSBMaterialDesign</a></td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Library Auth</td>
+                        <td><a href="https://github.com/benedmunds/CodeIgniter-Ion-Auth" target='_blank'>Codeiniger-Ion-Auth</a></td>
+                    </tr>
+                </tbody>
+        </table>
+        <p class="justify" data-aos="zoom-in-down" data-aos-duration="500">
+            <i>Data Text</i>
+        </p>
+    </div>
+    <div class="row">
+		<a id="contact" class="line"><h4 class="margin h1" data-aos="zoom-out" data-aos-duration="500">Kontak</h4></a>
+		<div class="col s12 m4">
+			<div class="icon-block">
+		    	<h2 class="center light-blue-text"><i class="material-icons" data-aos="zoom-in-down" data-aos-duration="500">location_on</i></h2>
+		    	<p class="center" data-aos="zoom-in-down" data-aos-duration="500">Alamat</p>
+		    	<p class="center" data-aos="zoom-in-down" data-aos-duration="500">ITATS SURABAYA</p>
+                <h2 class="center light-blue-text"><i class="material-icons" data-aos="zoom-in-down" data-aos-duration="500">phone</i></h2>
+                <p class="center" data-aos="zoom-in-down" data-aos-duration="500">(+62) xxxxxxx</p>
+
+		    	<h2 class="center light-blue-text"><i class="material-icons" data-aos="zoom-in-down" data-aos-duration="500">mail</i></h2>
+		    	<p class="center" data-aos="zoom-in-down" data-aos-duration="500">admin@gmail.com</p>
+		  	</div>
+	    </div>
+
+	    <div class="col s12 m8">
+	    	<div class="icon-block">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.559638417011!2d112.77696061432094!3d-7.290837673684796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa469f2b37b5%3A0x8d971ebe4e20a0d5!2sInstitut%20Teknologi%20Adhi%20Tama%20Surabaya!5e0!3m2!1sid!2sid!4v1606103164278!5m2!1sid!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> 
+            </div>
+	    </div>
+    </div>
+</div>
